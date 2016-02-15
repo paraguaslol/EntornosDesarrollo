e07a06a0910d12cf8e0ce4f1b2cce0830feb9103
violpe <victorolivarespeiro@gmail.com>
2016-02-15 17:14:58 +0100

    ex 02

---
+++ EntornosSegunda/src/pr02.java
@@ -0,0 +1,26 @@
+
+public class pr02 {
+
+public static void main(String[]args){
+	
+	String mensaje="Paquito es el tio mas guapo que conozco";
+	String mensajeCodificado="";
+	String temporal="";	
+	
+	mensajeCodificado=mensaje.toUpperCase();
+	
+	for(int i=0;i<mensajeCodificado.length();i=i+2){
+	if(i+1==mensajeCodificado.length()){
+		temporal=temporal+mensajeCodificado.charAt(i);
+	}else{
+		temporal=temporal+mensajeCodificado.charAt(i+1)+mensajeCodificado.charAt(i);
+		
+	}	
+	mensajeCodificado=temporal;
+	}
+	System.out.print(mensajeCodificado);
+}
+
+}
+
+
